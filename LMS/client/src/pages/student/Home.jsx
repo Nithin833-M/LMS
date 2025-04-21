b73e381e8dcddcfb8ab70203a3681a1f95bc5308
Nithin Chowdary <nithinman833@gmail.com>
2025-04-21 21:03:08 +0530

    deploy vercel

--- LMS/client/src/pages/student/Home.jsx
+++ LMS/client/src/pages/student/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Hero from '../../components/student/Hero';
 import Companies from '../../components/student/Companies';
 import CoursesSection from '../../components/student/CoursesSection';
-import TestimonialsSection from '../../components/student/testimonialsSection';
+import TestimonialsSection from '../../components/student/TestimonialsSection';
 import CallToAction from '../../components/student/CallToAction';
 import Footer from '../../components/student/Footer';
 
